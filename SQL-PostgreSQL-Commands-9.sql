f5258e0c118c7a5a2be9f5d4c7aba507b2b46c53
Isa Kayabasi <103357055+ISA-KAYABASI@users.noreply.github.com>
2022-10-08 17:04:25 +0300

    Update SQL-PostgreSQL-Commands-9.sql

--- SQL-PostgreSQL-Commands-9.sql
+++ SQL-PostgreSQL-Commands-9.sql
@@ -2,4 +2,13 @@
 SELECT city,country FROM city INNER JOIN country ON city.city_id = country.country_id;
 
 customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
-SELECT first_name, last_name from costumer INNER JOIN payment ON costumer.first_name, costumer.last_name = payment.first_name, payment.last_n
+SELECT customer.first_name, customer.last_name, payment.payment_id
+FROM customer
+INNER JOIN payment ON customer.customer_id = payment.customer_id;
+
+Write the INNER JOIN query where we can see the customer table and the rental_id in the rental table and the first_name and last_name names in the customer table together.
+SELECT rental.rantal_id, customer.first_name, cutomer.last_name
+FROM customer
+INNER JOIN rental ON rental.customer_id = customer.customer_id;
+
+
