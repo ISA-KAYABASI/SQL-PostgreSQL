59f0330151c856c3e43e1b9a44f6d2cda5752153
Isa Kayabasi <103357055+ISA-KAYABASI@users.noreply.github.com>
2022-10-08 11:46:56 +0300

    Update SQL-PostgreSQL-Commands-3.sql

--- SQL-PostgreSQL-Commands-3.sql
+++ SQL-PostgreSQL-Commands-3.sql
@@ -5,4 +5,7 @@ List the country names in the country column of the country table, consisting of
 SELECT * FROM country WHERE country ILIKE  '%_____n';
 
 In the title column of the movie table, list the movie names containing at least 4 'T' characters, regardless of upper or lower case letters.
-SELECT * FROM film WHERE ILIKE '%T%T%T%T%';
+SELECT title FROM film WHERE ILIKE '%T%T%T%T%';
+
+film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
+SELECT * FROM film WHERE (title LIKE 'C%') AND (length > 90) AND (rental_rate = 2.99) ;
