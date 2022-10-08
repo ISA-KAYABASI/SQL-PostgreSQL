3ccbbc82ac8ddbe638c03c9d4c88cbb167d5db59
Isa Kayabasi <103357055+ISA-KAYABASI@users.noreply.github.com>
2022-10-08 20:42:43 +0300

    Update SQL-PostgreSQL-Commands-12.sql

--- SQL-PostgreSQL-Commands-12.sql
+++ SQL-PostgreSQL-Commands-12.sql
@@ -5,4 +5,14 @@ How many movies have the highest rental_rate in the movie table?
 SELECT COUNT(length) FROM film WHERE rental_rate = (SELECT MAX(rental_rate)FROM film);
 
 film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
-SELECT COUNT(length) FROM
+SELECT title FROM film 
+WHERE
+(rental_rate = (SELECT MIN(rental_rate)FROM film))
+AND 
+(replacement_cost =
+(SELECT MIN(replacement_cost)FROM film));
+
+
+In the payment table, list the customers who make the most purchases.
+SELECT first_name, last_name, amount FROM customer FULL JOIN payment
+ON payment.customer_id = customer.customer_id ORDER BY amount DESC LIMIT 5;
