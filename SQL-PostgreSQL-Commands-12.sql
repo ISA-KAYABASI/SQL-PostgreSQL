24dcb6031f5a53593852318cc432cc8db9132998
Isa Kayabasi <103357055+ISA-KAYABASI@users.noreply.github.com>
2022-10-08 20:22:22 +0300

    Update SQL-PostgreSQL-Commands-12.sql

--- SQL-PostgreSQL-Commands-12.sql
+++ SQL-PostgreSQL-Commands-12.sql
@@ -1,2 +1,8 @@
 In the movie table, the movie length is shown in the length column. How many movies are longer than the average movie length?
 SELECT COUNT(length) FROM film WHERE length > (SELECT AVG(length) FROM film) ;
+
+How many movies have the highest rental_rate in the movie table?
+SELECT COUNT(length) FROM film WHERE rental_rate = (SELECT MAX(rental_rate)FROM film);
+
+film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
+SELECT COUNT(length) FROM
