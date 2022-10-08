383e0d3a1ff302654c82b1820c3339e7cec265ed
Isa Kayabasi <103357055+ISA-KAYABASI@users.noreply.github.com>
2022-10-08 11:44:34 +0300

    Update SQL-PostgreSQL-Commands-3.sql

--- SQL-PostgreSQL-Commands-3.sql
+++ SQL-PostgreSQL-Commands-3.sql
@@ -1,3 +1,8 @@
 List the country names in the country column of the country table, starting with the 'A' character and ending with the 'a' character.
-SELECT * FROM country WHERE country_name LIKE = 'A%a';
+SELECT * FROM country WHERE country LIKE  'A%a';
 
+List the country names in the country column of the country table, consisting of at least 6 characters and ending with the 'n' character.
+SELECT * FROM country WHERE country ILIKE  '%_____n';
+
+In the title column of the movie table, list the movie names containing at least 4 'T' characters, regardless of upper or lower case letters.
+SELECT * FROM film WHERE ILIKE '%T%T%T%T%';
