2dfe52500d41b823489463014a50558ab1a501ee
Isa Kayabasi <103357055+ISA-KAYABASI@users.noreply.github.com>
2022-10-08 20:16:04 +0300

    Update SQL-PostgreSQL-Commands-12.sql

--- SQL-PostgreSQL-Commands-12.sql
+++ SQL-PostgreSQL-Commands-12.sql
@@ -1,1 +1,2 @@
-
+In the movie table, the movie length is shown in the length column. How many movies are longer than the average movie length?
+SELECT COUNT(length) FROM film WHERE length > (SELECT AVG(length) FROM film) ;
