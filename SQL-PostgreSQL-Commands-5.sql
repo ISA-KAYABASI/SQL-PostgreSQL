c360ec1e9abceac41883b9c926f8ef5a8b474055
Isa Kayabasi <103357055+ISA-KAYABASI@users.noreply.github.com>
2022-10-08 13:49:57 +0300

    Update SQL-PostgreSQL-Commands-5.sql

--- SQL-PostgreSQL-Commands-5.sql
+++ SQL-PostgreSQL-Commands-5.sql
@@ -1,3 +1,8 @@
 film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en uzun (length) 5 filmi sıralayınız.
-SELECT * FROM film WHERE title LIKE '%n' AND length ORDER BY DESC LIMIT 5;
+SELECT * FROM film WHERE (title LIKE '%n') length ORDER BY DESC LIMIT 5;
 
+List the shortest (length) second(6,7,8,9,10) 5 movies(6,7,8,9,10) in the movie table and the movie title ends with the 'n' character.
+SELECT * FROM film WHERE (title LIKE '%n') ORDER BY length OFFSET 5 LIMIT 5;
+
+customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
+SELECT last_name FROM costumer WHERE last_name ORDER BY DESC LIMIT 4;
