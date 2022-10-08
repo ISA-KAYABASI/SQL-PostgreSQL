caca731ecfa3b02013796b2ef74d68abe19af0ec
Isa Kayabasi <103357055+ISA-KAYABASI@users.noreply.github.com>
2022-10-08 13:03:19 +0300

    Update SQL-PostgreSQL-Commands-5.sql

--- SQL-PostgreSQL-Commands-5.sql
+++ SQL-PostgreSQL-Commands-5.sql
@@ -1,1 +1,3 @@
+film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en uzun (length) 5 filmi sıralayınız.
+SELECT * FROM film WHERE title LIKE '%n' AND length ORDER BY DESC LIMIT 5;
 
